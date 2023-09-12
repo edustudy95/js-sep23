c2fd8ea7eb9d0f7dc73b15b011a9c3814ca570a4
edustudy95 <144672600+edustudy95@users.noreply.github.com>
2023-09-12 17:16:52 +0530

    varibles examples

---
+++ .devcontainer/01_basics/01_Variables.js
@@ -0,0 +1,37 @@
+// Example 1
+// const accountId = "12345"
+// let accountEmail = "rajan@gmail.com"
+// var accountPassword = "54321"
+// accountCity = "Bangaluru"
+// let accountState;
+
+// accountEmail = "gupta@gmail.com"
+// accountPassword = "123321"
+// accountCity = "mfp"
+
+// console.log([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+//Example 2
+// const bankAccount ="SBI"
+// let bankBranch ="musahari"
+// let bankIfsc = "sbio123"
+// let bankCity = "mfp"
+
+//  bankBranch ="ANAND VIHAR"
+//  bankIfsc = "SBI03214"
+//  bankCity = "NEW DELHI "
+
+// console.log([bankAccount, bankBranch, bankIfsc, bankIfsc]);
+// console.table([bankAccount, bankBranch, bankIfsc, bankCity]);
+
+// Example 3
+const Degree = "Engineering"
+let stdName ="Rajan"
+let stdMarks ="123"
+let stdBranch = "EEE"
+
+stdName = "Rajan Gupta"
+stdMarks = "300"
+stdBranch = "Electrical"
+
+console.table([Degree, stdName, stdMarks, stdBranch])
