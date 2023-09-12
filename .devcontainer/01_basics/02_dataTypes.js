646bf87b23b48440f49b02d74a5f9790020bdd80
edustudy95 <144672600+edustudy95@users.noreply.github.com>
2023-09-12 17:17:56 +0530

    DataTypes Examples

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+let name = "Rajan"   // {its datatype is STRING}
+
+let age = 18     // {its datatype is NUMBER}
+
+let isLoggedIn = false   // {its datatype is BOOLEAN}
+
+let state;          // {its datatype is UNDEFINED}
+
+// Premitive Data type
+    // number => 2 to the power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => its standalone value     {its datatype is OBJECT}
+    // undefined => value not assigned  {its datatype is UNDEFINED}
+    // symbol => unique
+
+
+// Object Data type
+
+console.log(typeof null);       // {its datatype is OBJECT}
+console.log(typeof undefined);  // {its datatype is UNDEFINED}
+console.log(typeof state);
