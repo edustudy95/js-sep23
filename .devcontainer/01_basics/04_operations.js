125fa03cb76b413ad00685b46e7fd7e3fb3b30b3
edustudy95 <144672600+edustudy95@users.noreply.github.com>
2023-09-13 14:05:53 +0530

    Number, String, Prefix, Postfix Operation Examples

---
+++ .devcontainer/01_basics/04_operations.js
@@ -0,0 +1,50 @@
+/*   Number, String, Prefix,  Postfix Operation Examples    */
+
+// let str1 = "rajan"
+// let str2 = " gupta"
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// let num1 = 1
+// let str1 = "2"
+
+// let sum = 1 + "2"
+// console.log(sum);      
+// console.log(typeof sum); // Number + String = string
+
+// let sum = "1"+ 2
+// console.log(sum);      
+// console.log(typeof sum);     
+
+// let sum = "1"+ "2"
+// console.log(sum);
+// console.log(typeof sum);    
+
+// console.log("rajan" + 2 + 1);        // o/p=>  rajan21
+
+// console.log(1 + 2 + "rajan" );         // o/p=>  3rajan
+
+// console.log(1 + 2 + "rajan" + 4);      // o/p=> 3rajan4
+
+
+
+// -------------prefix   &   postfix ----------------
+
+let gameCounter = 100
+// gameCounter++     // postfix
+// ++gameCounter     // prefix
+// console.log(gameCounter);
+
+// postfix Examples
+let x = 3
+let y = x++
+// console.log(x);
+// console.log(y);
+
+
+// prefix Examples
+let a = 2
+let b = ++a
+console.log(a);
+console.log(b);
