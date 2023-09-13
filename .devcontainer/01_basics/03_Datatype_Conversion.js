39598445bca4959932c18f7f23e120879fee4317
edustudy95 <144672600+edustudy95@users.noreply.github.com>
2023-09-13 12:56:49 +0530

    Datatype Conversion

---
+++ .devcontainer/01_basics/03_Datatype_Conversion.js
@@ -0,0 +1,40 @@
+       /*------EXAMPLES --------*/
+// let score = "20"
+
+// console.log(score);
+
+// let isNumber = Number(score)
+// console.log(isNumber);
+// console.log(typeof isNumber);
+
+
+
+       /*------EXAMPLES --------*/
+// let marks = "20abc"
+// console.log(marks);
+// console.log(typeof marks);
+
+// let isNumber = Number(marks)
+
+// console.log(isNumber);
+// console.log(typeof isNumber);
+
+
+       /*------EXAMPLES --------*/
+// let someNumber = 20
+
+// let someString = String(someNumber)
+// console.log(someString);
+// console.log(typeof someString);
+
+
+
+       /*------EXAMPLES --------*/
+let isNumber = undefined
+console.log(isNumber);
+console.log(typeof isNumber);
+
+let isBoolean  = Boolean(isNumber)
+console.log(isBoolean);
+
+
